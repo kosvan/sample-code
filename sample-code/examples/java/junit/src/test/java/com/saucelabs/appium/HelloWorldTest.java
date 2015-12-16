5fbd0809663690e41741b46a31db5548b6617858
Ivan Gromov <ivangromov@Mac-mini-Ivan.local>
2015-12-16 17:18:30 +0300

    тестовый тест

---
+++ sample-code/examples/java/junit/src/test/java/com/saucelabs/appium/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.saucelabs.appium;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+@SuppressWarnings("deprecation")
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		System.out.println("Hello Wrold test");
+	}
+
+}
